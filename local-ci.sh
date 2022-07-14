fae49ea53ce3d2693f2d5f5cc65a9f7be39714bd
obi-talentnet <omerenu@talentnet.com>
2022-07-14 10:47:02 -0600

    update file link 2

--- local-ci.sh
+++ local-ci.sh
@@ -5,7 +5,8 @@
 
 # @TODO copy from local ci-repo in future
 # split by node vs php project
-# cp tn-hooks/pre-commit.sample  .git/hooks/pre-commit
-cp https://raw.githubusercontent.com/obi-talentnet/local-ci/setupJs/tn-hooks/pre-commit.sample .git/hooks/pre-commit
+# 
+wget https://raw.githubusercontent.com/obi-talentnet/local-ci/setupJs/tn-hooks/pre-commit.sample 
+mv pre-commit.sample  .git/hooks/pre-commit 
 chmod +x .git/hooks/pre-commit
 echo "Local CI has been successfully installed !"
--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# setup script
-
-
- 
-echo "Local CI is working!"
