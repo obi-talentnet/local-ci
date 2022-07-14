75260cad8d53acc64d3f1b8efe792c503b54d854
obi-talentnet <omerenu@talentnet.com>
2022-07-14 11:02:19 -0600

    Small changes, :wrench:

--- local-ci.sh
+++ local-ci.sh
@@ -3,10 +3,19 @@
 
 
 
-# @TODO copy from local ci-repo in future
-# split by node vs php project
-# 
-wget https://raw.githubusercontent.com/obi-talentnet/local-ci/setupJs/tn-hooks/pre-commit.sample 
+read -p "Is this for a PHP project or JS? (choices: js | php) "  email
+echo
+if [$email = "js" ]; then
+    wget https://raw.githubusercontent.com/obi-talentnet/local-ci/setupJs/tn-hooks/pre-commit-js.sample 
+else
+    wget https://raw.githubusercontent.com/obi-talentnet/local-ci/setupJs/tn-hooks/pre-commit-php.sample 
+fi
+
+
+
+
+wget https://raw.githubusercontent.com/obi-talentnet/local-ci/setupJs/tn-hooks/pre-commit-js.sample 
+
 mv pre-commit.sample  .git/hooks/pre-commit 
 chmod +x .git/hooks/pre-commit
 echo "Local CI has been successfully installed !"
